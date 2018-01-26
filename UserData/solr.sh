971879deb2a6c462a438095d66d8c2f0cf7b68c6
Favenzio Calvo <fcalvo@fsu.edu>
2018-01-26 16:27:37 -0500

    Began work to install Solr application

--- UserData/solr.sh
+++ UserData/solr.sh
@@ -1,23 +1,31 @@
 yum -y update > /root/updates.txt
 
+# Mount external devices 
 mkfs -t ext4 /dev/xvdb
 mkfs -t ext4 /dev/xvdc
-
 cp -pr /var /tmp
 cp -pr /home /tmp 
-
 mount /dev/xvdb /var
 mount /dev/xvdc /home
-
 cp -pr /etc/fstab /etc/fstab.orig
-
 echo "/dev/xvdb   /var        ext4    defaults,nofail 0   2" >> /etc/fstab
 echo "/dev/xvdc   /home       ext4    defaults,nofail 0   2" >> /etc/fstab
-
 mount -a
-
 cp -prT /tmp/var /var
 cp -prT /tmp/home /home
-
 rm -rf /tmp/var
 rm -rf /tmp/home
+
+# Set timezone
+rm -f /etc/localtime
+cd /etc
+ln -s /usr/share/zoneinfo/US/Eastern localtime
+
+# Upgrade java
+yum -y install java-1.8.0 >> /root/updates.txt
+yum -y remove java-1.7.0-openjdk >> /root/updates.txt
+
+# Install Solr and configure it to auto start on boot
+cd /root
+wget http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/7.2.1/solr-7.2.1.tgz
+#tar xzf solr-7.2.1.tgz solr-7.2.1/bin/install_solr_service.sh --strip-components=2 
