c3317d5231ed01e6d8d0bc95bdda46dabe50ed67
Favenzio <fcalvo@fsu.edu>
2018-02-01 14:15:19 -0500

    Added drupaldb creation logging

--- UserData/drupal.sh
+++ UserData/drupal.sh
@@ -32,7 +32,7 @@ yum -y install httpd mysql ImageMagick > /root/installs.txt
 yum -y install php php-devel php-gd php-xml php-soap php-mysql php-mbstring > /root/installs.php.txt
 
 # Configure MySQL
-mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "CREATE DATABASE drupaldb;"
+mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "CREATE DATABASE drupaldb;" > /root/mysql.log
 mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "CREATE USER '${DRUPAL_DATABASE_USER}'@'10.50.0.101' IDENTIFIED BY '${DRUPAL_DATABASE_PASS}';"
 mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "GRANT ALL PRIVILEGES ON drupaldb.* TO ${DRUPAL_DATABASE_USER}@10.50.0.101;"
 mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "FLUSH PRIVILEGES;"
@@ -47,7 +47,7 @@ rm -rf /var/www/html
 /root/.composer/vendor/bin/drush dl drupal-7.x --destination=/var/www/ --drupal-project-rename=html
 cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
 /root/.composer/vendor/bin/drush --root=/var/www/html --uri=default -y si standard --account-name=admin --account-pass=admin --db-url=mysql://$DRUPAL_DATABASE_USER:$DRUPAL_DATABASE_PASSWORD@10.50.0.101/drupaldb --site-name=Islandora
-mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "ALTER DATABASE islandoradb CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+mysql -u $DATABASE_ROOT_USER -p$DATABASE_ROOT_PASSWORD -h $DATABASE_ENDPOINT -e "ALTER DATABASE drupaldb CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
 sed -i -e "s/'prefix'\ =>\ '',/'prefix'\ =>\ '',\ 'charset'\ =>\ 'utf8mb4',\ 'collation'\ =>\ 'utf8mb4_general_ci',/g" /var/www/html/sites/default/settings.php
 chmod -R 777 /var/www/html
 
