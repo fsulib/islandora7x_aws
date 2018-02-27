1c15e4a6ad701c01fbf5c508481650a18e78f92e
Bryan J. Brown <bryjbrown@gmail.com>
2018-02-27 14:53:18 -0500

    The worlds softest refactor

--- UserData/drupal.sh
+++ UserData/drupal.sh
@@ -68,13 +68,13 @@ sed -i -e 's/AllowOverride\ None/AllowOverride\ All/g' /etc/httpd/conf/httpd.con
 service httpd restart
 
 # Install core Islandora modules
-wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/UserData/core_islandora_modules.txt -O /tmp/core_islandora_modules.txt
-while read line
-do
-  cd /var/www/html/sites/all/modules/
-  git clone https://github.com/Islandora/$line
-  # /root/.composer/vendor/bin/drush -y --root=/var/www/html en $line
-done < /tmp/core_islandora_modules.txt
+#wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/UserData/core_islandora_modules.txt -O /tmp/core_islandora_modules.txt
+#while read line
+#do
+#  cd /var/www/html/sites/all/modules/
+#  git clone https://github.com/Islandora/$line
+#  # /root/.composer/vendor/bin/drush -y --root=/var/www/html en $line
+#done < /tmp/core_islandora_modules.txt
 
 # Download tuque library and enable libraries module
 cd /var/www/html/sites/all/libraries
@@ -83,13 +83,19 @@ cd /var/www/html
 /root/.composer/vendor/bin/drush en libraries -y
 
 # Set Fedora URL and enable Islandora
+cd /var/www/html/sites/all/modules
+git clone https://github.com/Islandora/islandora.git
 /root/.composer/vendor/bin/drush vset islandora_base_url "http://10.50.0.102:8080/fedora"
 /root/.composer/vendor/bin/drush --user=1 en islandora -y
 
 # Enable the Basic Collection module
+cd /var/www/html/sites/all/modules
+git clone https://github.com/Islandora/islandora_solution_pack_collection.git
 /root/.composer/vendor/bin/drush --user=1 en islandora_basic_collection -y
 
 # Enable the Basic Image module
+cd /var/www/html/sites/all/modules
+git clone https://github.com/Islandora/islandora_solution_pack_image.git
 /root/.composer/vendor/bin/drush dl imagemagick
 /root/.composer/vendor/bin/drush en imagemagick -y
 /root/.composer/vendor/bin/drush --user=1 en islandora_basic_image -y
