38224d55ea93560283ba09a9a88c882a5756e3a8
Favenzio Calvo <fcalvo@fsu.edu>
2018-02-27 14:48:42 -0500

    chmod gsearch install file

--- UserData/fedora.sh
+++ UserData/fedora.sh
@@ -105,11 +105,12 @@ cd /root
 wget https://github.com/discoverygarden/gsearch/releases/download/v2.8.1/fedoragsearch-2.8.1.zip
 unzip fedoragsearch-2.8.1.zip
 /bin/cp -v fedoragsearch-2.8.1/fedoragsearch.war /var/lib/tomcat7/webapps
+chown tomcat:tomcat /var/lib/tomcat7/webapps/fedoragsearch.war
 wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/UserData/fedora-users.xml
 perl -i -p -e 's/GSearchPass/$ENV{FEDORA_ADMIN_PASS}/g' fedora-users.xml
 /bin/cp -f fedora-users.xml $FEDORA_HOME/server/config
 
-service tomcat7 restart
+service tomcat7 restart >> /root/installs.txt 2>&1
 
 # Configure Fedora GSearch
 wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/UserData/fgsconfig-basic-for-islandora.properties
