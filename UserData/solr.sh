242afa2a27748d79dd3e29fc398cb4ce257ce005
Favenzio Calvo <fcalvo@fsu.edu>
2018-02-28 14:42:44 -0500

    Dowload and unpack solr

--- UserData/solr.sh
+++ UserData/solr.sh
@@ -21,9 +21,21 @@ ln -s /usr/share/zoneinfo/US/Eastern localtime
 
 
 # Run updates & installations 
-yum -y update > /root/updates.txt
+yum -y update > /root/updates.txt 2>&1
+yum -y install tomcat7 >> /root/installs.txt 2>&1
+
+# Setup variables needed by Solr
+export PATH=$PATH:$JAVA_HOME/bin
+export CATALINA_HOME=/var/lib/tomcat7
+export CATALINA_BASE=/var/lib/tomcat7
+export CLASSPATH=$JAVA_HOME/lib
 
 # Install Solr and configure it to auto start on boot
 cd /root
-wget https://archive.apache.org/dist/lucene/solr/4.6.1/solr-4.6.1.tgz
-wget https://raw.githubusercontent.com/fcrepo3/gsearch/master/FgsConfig/FgsConfigIndexTemplate/Solr/conf/schema-4.6.1-for-fgs-2.8.xml
+wget https://archive.apache.org/dist/lucene/solr/4.6.1/solr-4.6.1.tgz >> /root/solrinstall.txt 2>&1
+wget https://raw.githubusercontent.com/fcrepo3/gsearch/master/FgsConfig/FgsConfigIndexTemplate/Solr/conf/schema-4.6.1-for-fgs-2.8.xml >> /root/solrinstall.txt 2>&1
+tar -xzvf solr-4.6.1.tgz >> /root/solrinstall.txt 2>&1
+cp -v /root/solr-4.6.1/dist/solr-4.6.1.war /var/lib/tomcat7/webapps/solr.war >> /root/solrinstall.txt 2>&1
+/usr/bin/unzip -o /var/lib/tomcat7/webapps/solr.war -d /var/lib/tomcat7/webapps/solr/
+mkdir -p /usr/local/solr >> /root/solrinstall.txt 2>&1
+cp -r /root/solr-4.6.1/example/solr/. /usr/local/solr/ >> /root/solrinstall.txt 2>&1
